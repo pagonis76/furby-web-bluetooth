cd4d0b24291e0f193fedfe4939acc0755ae728b4
pagonis76 <155277076+pagonis76@users.noreply.github.com>
2026-07-10 16:28:28 -0500

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -15,10 +15,10 @@
         ]
     },
     {
-        "file": "noimg2.dlc", 
-        "title": "Audio only",
+        "file": "tu011090.dlc, 
+        "title": "Partyrockithink",
         "buttons": [
-            { "title": "Audio test ", "action": [75,0,4,4] }
+            { "title": "idk ", "action": [75,0,4,4] }
         ]
     },
     {
